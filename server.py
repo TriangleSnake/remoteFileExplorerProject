f4508237790b1bd84244b27ad2ec058d56dd8449
TriangleSnake <trianglesnake2002@gmail.com>
2022-12-09 21:21:33 +0800

    upload server

---
+++ server.py
@@ -0,0 +1,10 @@
+import socket
+import os
+HOST='127.0.0.1'
+PORT=8080
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((HOST,PORT))
+s.listen(5)
+while True:
+    client,addr=s.accept()
+    print("123")
